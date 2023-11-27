5ec09b5d7b174cf193907b9037917b3bdc8ea84f
Eduardo <eduardoborgesgouveia@gmail.com>
2023-11-27 17:27:02 -0300

    --ajustes para funcionar sempre

--- server_winzMQ/file_transfer_zeroMQ_invertido.py
+++ server_winzMQ/file_transfer_zeroMQ_invertido.py
@@ -4,8 +4,9 @@
 #  large chunks with no attempt at flow control.
 
 from __future__ import print_function
-from threading import Thread
-
+import os
+import time
+from utils.threadHandler import ThreadHandler
 import zmq
 
 from zhelpers import socket_set_hwm, zpipe
@@ -41,41 +42,53 @@ def client_thread(ctx):
 # The server thread reads the file from disk in chunks, and sends
 # each chunk to the client as a separate message. We only have one
 # test file, so open that once and then serve it out as needed:
+def server_thread():
+    global start_time,ctx
+    if ((time.time() - start_time) > 1):
+        
+        router = ctx.socket(zmq.DEALER)
 
-
-def server_thread(ctx,pipe):
-
-    router = ctx.socket(zmq.DEALER)
-
-    # Default HWM is 1000, which will drop messages here
-    # since we send more than 1,000 chunks of test data,
-    # so set an infinite HWM as a simple, stupid solution:
-    socket_set_hwm(router, 0)
-    router.bind("tcp://*:6000")
-    router.send(b"fetch")
-    total = 0       # Total bytes received
-    chunks = 0      # Total chunks received
-    
-    filename = "dados_recebidos.csv"
-    with open(filename, 'wb') as f:
+        # Default HWM is 1000, which will drop messages here
+        # since we send more than 1,000 chunks of test data,
+        # so set an infinite HWM as a simple, stupid solution:
+        socket_set_hwm(router, 0)
+        router.bind("tcp://*:6000")
+        # router.RCVTIMEO = 1000
+        total = 0       # Total bytes received
+        chunks = 0      # Total chunks received
+        start_time = time.time()
+        print("esperando...")
+        filename = "dados_recebidos.csv"
+        # os.remove(filename)
+        chunks_vetor = []
         while True:
-            try:
-                chunk = router.recv()
-                f.write(chunk)
-            except zmq.ZMQError as e:
-                if e.errno == zmq.ETERM:
-                    return   # shutting down, quit
-                else:
-                    raise
+            if router.poll(1000, zmq.POLLIN):
+                try:
+                    chunk = router.recv()
+                    print("recebendo...")
+                    chunks_vetor.append(chunk)
+                    
+                except zmq.ZMQError as e:
+                    if e.errno == zmq.ETERM:
+                        return   # shutting down, quit
+                    else:
+                        raise
 
-            chunks += 1
-            size = len(chunk)
-            total += size
-            if size == 0:
-                break   # whole file received
+                chunks += 1
+                size = len(chunk)
+                total += size
+                if size == 0:
+                    with open(filename, "wb") as f:
+                        for i in chunks_vetor:
+                            f.write(i)
+                    break   # whole file received
+            else:
+                router.send(b"fetch")
+                print("error: message timeout")
 
-    print ("%i chunks received, %i bytes" % (chunks, total))
-    pipe.send(b"OK")
+        print ("%i chunks received, %i bytes" % (chunks, total))
+        
+        # pipe.send(b"OK")
 
 
     
@@ -86,23 +99,25 @@ def server_thread(ctx,pipe):
 # processes:
 
 def main():
-
+    global start_time,ctx
     # Start child threads
     ctx = zmq.Context()
     a,b = zpipe(ctx)
+    start_time = time.time()
 
-    client = Thread(target=client_thread, args=(ctx,))
-    server = Thread(target=server_thread, args=(ctx,b))
+
+    # client = Thread(target=client_thread, args=(ctx,))
+    server = ThreadHandler(server_thread)
     #client.start()
     server.start()
-
-    # loop until client tells us it's done
-    try:
-        print (a.recv())
-    except KeyboardInterrupt:
-        pass
-    del a,b
-    ctx.term()
+    input("aperte ctrl+c para sair\n")
+    # # loop until client tells us it's done
+    # try:
+    #     print (a.recv())
+    # except KeyboardInterrupt:
+    #     pass
+    # del a,b
+    # ctx.term()
 
 if __name__ == '__main__':
     main()
